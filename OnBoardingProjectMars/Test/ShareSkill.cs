85a7ab171e6585983dc81fa49452ddcf02913bd1
Anjali Bhama Sasikumar <anjalibs@gmail.com>
2022-08-31 17:53:54 +0800

    POM

---
+++ OnBoardingProjectMars/Test/ShareSkill.cs
@@ -0,0 +1,23 @@
+﻿
+using MarsTest.POM;
+using MarsTest.Utilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsTest.Test
+{
+    [TestFixture]
+    public class ShareSkill : CommonDriver
+    {
+        [Test]
+        public void TestShareSkill()
+        {
+            var SignInPage = new SignInPage(driver);
+            SignInPage.NavigateToShareSkillHomePage();
+        }
+    }
+}
